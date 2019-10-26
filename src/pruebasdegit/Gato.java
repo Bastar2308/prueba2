2e96a4cb8b904625cc06eb504f17d8cfc4fa344a
thelegendkiller <52903659+thelegendkiller@users.noreply.github.com>
2019-10-26 16:01:40 -0500

    Hice un gato (eso creo)

---
+++ src/pruebasdegit/Gato.java
@@ -0,0 +1,33 @@
+package pruebasdegit;
+
+public class Gato {
+    private String nombre;
+    private String raza;
+    private String edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+    
+    
+}
